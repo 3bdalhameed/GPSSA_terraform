1c1c9fd204de4187e6d0bdcb447d907996cf58f4
3bdalhameed <abdalhmeed.dradkeh@gmail.com>
2026-04-26 13:26:00 +0300

    .

--- terraform/shared/modules/aks/main.tf
+++ terraform/shared/modules/aks/main.tf
@@ -28,6 +28,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     dns_service_ip    = "10.0.0.10"
   }
 
+  oidc_issuer_enabled               = true
   role_based_access_control_enabled = true
 
   identity {
